96536f2b429e7d12d6ae0dc3cf389a014e422617
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-07-15 15:52:33 -0600

    Enable asynchronous mode

--- jupyter_forward/core.py
+++ jupyter_forward/core.py
@@ -85,7 +85,7 @@ def start(
     session = Connection(host, connect_kwargs={'password': password})
     # _ = session.run(f'ls -ltrh {notebook_dir}')
     command = f'conda activate {conda_env} &&  jupyter lab --no-browser --ip=`hostname` --port={port} --notebook-dir={notebook_dir}'
-    _ = session.run(command)
+    _ = session.run(command, asynchronous=True)
 
 
 @app.command()
