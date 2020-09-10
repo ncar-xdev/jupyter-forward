20ec96d22e9191af5497e817fee7d07bd9aa8c0b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-09-10 03:47:52 -0600

    Remove config command for the time being

--- jupyter_forward/core.py
+++ jupyter_forward/core.py
@@ -1,4 +1,3 @@
-import dataclasses
 import getpass
 import random
 import socket
@@ -11,26 +10,6 @@ import typer
 from fabric import Connection
 
 app = typer.Typer(help='Jupyter Lab Port Forwarding Utility')
-
-
-@dataclasses.dataclass
-class Config:
-    host: str
-    hostname: str
-    username: str
-
-    def run(self):
-        template = f"""
-Host {self.host}
-    User {self.username}
-    Hostname {self.hostname}
-    GSSAPIDelegateCredentials yes
-    GSSAPIAuthentication yes
-    ControlMaster auto
-    ControlPersist yes
-    ControlPath ~/.ssh/control/%C
-        """
-        print(template)
 
 
 def open_browser(port: int = None, token: str = None, url: str = None):
@@ -129,38 +108,6 @@ def parse_stdout(stdout: str):
                 token = result.query.split('token=')[-1].strip()
             break
     return {'hostname': hostname, 'port': port, 'token': token, 'url': url}
-
-
-@app.command()
-def config(host: str, username: str, hostname: str = typer.Option(None, show_default=True)):
-    """
-    Prints an ssh configuration for the user, selecting a
-    login node at random if host has multiple login nodes.
-    """
-    random.seed(42)
-
-    Machine = namedtuple('Machine', ['domain', 'login_node'])
-
-    machines = {
-        'cheyenne': Machine('ucar.edu', random.choice(range(1, 7))),
-        'casper': Machine('ucar.edu', random.choice((6, 26))),
-        'hobart': Machine('cgd.ucar.edu', None),
-    }
-
-    if not hostname:
-        try:
-            m = machines[host]
-            if m.login_node:
-                hostname = f'{host}{m.login_node}.{m.domain}'
-            else:
-                hostname = f'{host}.{m.domain}'
-        except KeyError:
-            raise ValueError(
-                f'Unable to find hostname information for `{host}` in the list of registered hosts: {list(machines.keys())}. Specify hostname via --hostname option.'
-            )
-
-    config = Config(host, hostname, username)
-    config.run()
 
 
 @app.command()
