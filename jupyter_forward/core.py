6aab4c80e15f6bd6c1915c6c83a654cbfe1681c0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-01-30 05:34:21 -0700

    Pretty print logs with rich (#58)

--- jupyter_forward/core.py
+++ jupyter_forward/core.py
@@ -1,12 +1,26 @@
 import datetime
 import getpass
 import socket
+import sys
 import time
 from dataclasses import dataclass
 
 import invoke
 import paramiko
 from fabric import Connection
+from rich.console import Console
+
+console = Console()
+
+
+def _authentication_handler(title, instructions, prompt_list):
+    """
+    Handler for paramiko auth_interactive_dumb
+    """
+    resp = []
+    for pr in prompt_list:
+        resp.append(getpass.getpass(str(pr[0])))
+    return resp
 
 
 @dataclass
@@ -37,23 +51,42 @@ class RemoteRunner:
 
     def __post_init__(self):
         if self.port_forwarding and not is_port_available(self.port):
-            raise SystemExit(
-                (
-                    f'''Specified port={self.port} is already in use on your local machine. Try a different port'''
-                )
+            console.log(
+                f'''[bold red]Specified port={self.port} is already in use on your local machine. Try a different port'''
             )
+            sys.exit(1)
 
         connect_kwargs = {}
         if self.identity:
             connect_kwargs['key_filename'] = [self.identity]
 
         self.session = Connection(self.host, connect_kwargs=connect_kwargs, forward_agent=True)
+        console.log(
+            f'[bold cyan]Authenticating user ({self.session.user}) from client ({socket.gethostname()}) to remote host ({self.session.host})'
+        )
+        # Try passwordless authentication
         try:
             self.session.open()
-        except paramiko.ssh_exception.BadAuthenticationType:
-            loc_transport = self.session.client.get_transport()
-            loc_transport.auth_interactive_dumb(self.session.user, _authentication_handler)
-            self.session.transport = loc_transport
+        except (
+            paramiko.ssh_exception.BadAuthenticationType,
+            paramiko.ssh_exception.AuthenticationException,
+        ):
+            pass
+
+        # Prompt for password and token (2FA)
+        if not self.session.is_connected:
+            for _ in range(2):
+                try:
+                    loc_transport = self.session.client.get_transport()
+                    loc_transport.auth_interactive_dumb(self.session.user, _authentication_handler)
+                    self.session.transport = loc_transport
+                    break
+                except Exception:
+                    console.log('[bold red]❌ Failed to Authenticate your connection')
+            if not self.session.is_connected:
+                sys.exit(1)
+
+        console.log('✅ [bold cyan]The client is authenticated successfully')
 
     def dir_exists(self, directory):
         """
@@ -68,9 +101,7 @@ class RemoteRunner:
         """
         Sets up SSH port forwarding
         """
-        print('**********************************')
-        print('*** Setting up port forwarding ***')
-        print('**********************************\n\n')
+        console.rule('[bold green]Setting up port forwarding', characters='*')
         local_port = int(self.port)
         remote_port = int(self.parsed_result['port'])
         with self.session.forward_local(
@@ -129,21 +160,24 @@ class RemoteRunner:
                 self.session.run(f'chmod +x {script_file}', **kwargs)
                 command = f'{self.launch_command} {script_file}'
 
-            self.session.run(command, asynchronous=True, **kwargs, echo=True)
+            self.session.run(command, asynchronous=True, **kwargs, echo=False)
 
             # wait for logfile to contain access info, then write it to screen
             condition = True
             stdout = None
             pattern = 'is running at:'
-            while condition:
-                try:
-                    result = self.session.run(f'cat {self.log_file}', **kwargs)
-                    if pattern in result.stdout:
-                        condition = False
-                        stdout = result.stdout
-                except invoke.exceptions.UnexpectedExit:
-                    print(f'Trying to access {self.log_file} on {self.session.host} again...')
-                    pass
+            with console.status(
+                f'[bold cyan]Parsing {self.log_file} log file on {self.session.host} for jupyter information',
+                spinner='weather',
+            ):
+                while condition:
+                    try:
+                        result = self.session.run(f'cat {self.log_file}', **kwargs, echo=False)
+                        if pattern in result.stdout:
+                            condition = False
+                            stdout = result.stdout
+                    except invoke.exceptions.UnexpectedExit:
+                        pass
             self.parsed_result = parse_stdout(stdout)
 
             if self.port_forwarding:
@@ -151,11 +185,13 @@ class RemoteRunner:
             else:
                 open_browser(url=self.parsed_result['url'])
                 self.session.run(f'tail -f {self.log_file}', **kwargs)
-        finally:
+        except Exception:
             self.close()
-            print('\n***********************************************************')
-            print('*** Terminated the network connection to the remote end ***')
-            print('***********************************************************\n')
+
+        finally:
+            console.rule(
+                '[bold red]Terminated the network 📡 connection to the remote end', characters='*'
+            )
 
 
 def open_browser(port: int = None, token: str = None, url: str = None):
@@ -185,9 +221,10 @@ def open_browser(port: int = None, token: str = None, url: str = None):
         url = f'http://localhost:{port}'
         if token:
             url = f'{url}/?token={token}'
-    print('******************************************************')
-    print(f'*** Opening Jupyter Lab interface in a browser at ***\n\t===> {url} <===')
-    print('******************************************************\n\n')
+
+    console.rule('[bold green]Opening Jupyter Lab interface in a browser', characters='*')
+    console.log(f'Jupyter Lab URL: {url}')
+    console.rule('[bold green]', characters='*')
     webbrowser.open(url, new=2)
 
 
@@ -232,13 +269,3 @@ def parse_stdout(stdout: str):
                 token = result.query.split('token=')[-1].strip()
             break
     return {'hostname': hostname, 'port': port, 'token': token, 'url': url}
-
-
-def _authentication_handler(title, instructions, prompt_list):
-    """
-    Handler for paramiko auth_interactive_dumb
-    """
-    resp = []
-    for pr in prompt_list:
-        resp.append(getpass.getpass(str(pr[0])))
-    return resp
