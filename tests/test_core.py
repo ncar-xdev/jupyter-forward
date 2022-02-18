b17e88a12ad1c028c740968669dbe8b3fe0d1348
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-02-17 18:03:45 -0700

    Parametrize Shells

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,14 +9,15 @@ NOT_GITHUB_ACTIONS = os.environ.get('GITHUB_ACTIONS') is None
 requires_gha = pytest.mark.skipif(NOT_GITHUB_ACTIONS, reason='requires GITHUB_ACTIONS')
 
 
-@pytest.fixture(scope='session', params=[f"{os.environ['USER']}@eniac.local"])
+@pytest.fixture(scope='session')
 def runner(request):
-    remote = jupyter_forward.RemoteRunner(request.param)
+    remote = jupyter_forward.RemoteRunner(f"{os.environ['USER']}@eniac.local", shell=request.param)
     yield remote
     remote.close()
 
 
 @requires_gha
+@pytest.mark.parametrize('runner', ['bash', 'sh', 'tcsh', 'zsh'], indirect=True)
 def test_connection(runner):
     USER = os.environ['USER']
     assert runner.session.is_connected
@@ -26,6 +27,7 @@ def test_connection(runner):
 
 @requires_gha
 @pytest.mark.parametrize('command', ['echo $HOME'])
+@pytest.mark.parametrize('runner', ['bash', 'sh', 'tcsh', 'zsh'], indirect=True)
 def test_run_command(runner, command):
     out = runner.run_command(command)
     assert not out.failed
@@ -34,6 +36,7 @@ def test_run_command(runner, command):
 
 @requires_gha
 @pytest.mark.parametrize('command', ['echod $HOME'])
+@pytest.mark.parametrize('runner', ['bash', 'sh', 'tcsh', 'zsh'], indirect=True)
 def test_run_command_failure(runner, command):
     out = runner.run_command(command, exit=False)
     assert out.failed
@@ -44,6 +47,7 @@ def test_run_command_failure(runner, command):
 
 
 @requires_gha
+@pytest.mark.parametrize('runner', ['bash', 'sh', 'tcsh', 'zsh'], indirect=True)
 def test_set_logs(runner):
     runner._set_log_directory()
     assert runner.log_dir == f"{os.environ['HOME']}/.jupyter_forward"
