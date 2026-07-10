01281835a603f5642cd2399e006db68ed057128f
Justus Magin <keewis@posteo.de>
2026-07-10 14:50:29 +0200

    better documentation for the new options

--- jupyter_forward/cli.py
+++ jupyter_forward/cli.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pathlib import Path
 
 import typer
@@ -5,6 +6,13 @@ import typer
 from .core import RemoteRunner
 
 app = typer.Typer(help='Jupyter Lab Port Forwarding Utility')
+
+
+class EnvironmentManager(str, Enum):
+    micromamba = 'micromamba'
+    mamba = 'mamba'
+    conda = 'conda'
+    pixi = 'pixi'
 
 
 def version_callback(value: bool):
@@ -23,16 +31,16 @@ def start(
         help=(
             """The local port the remote notebook server will be forwarded to. If not specified, defaults to 8888."""
         ),
-        show_default=True,
     ),
-    env_manager: str = typer.Option(
-        None, '--environment-manager', show_default=True, help='Name of the environment manager.'
+    env_manager: EnvironmentManager | None = typer.Option(
+        None,
+        '--environment-manager',
+        help='Name of the environment manager. If not specified will check the conda-like managers in sequence.',
     ),
     env_manager_path: str = typer.Option(
         None,
         '--environment-manager-path',
-        show_default=True,
-        help='Path of the environment manager.',
+        help="Path of the environment manager. If not specified, it will be inferred from the environment manager's name.",
     ),
     conda_env: str = typer.Option(
         None,
